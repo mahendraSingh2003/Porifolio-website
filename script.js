9ea446e60cbed21540a4c9eb011eb6ec9c60811b
Mahendra Singh Gurjar <157353148+mahendraSingh2003@users.noreply.github.com>
2024-06-19 13:26:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
 function myMenuFunction(){
-    var menuBtn = document.getElementById("myNavMenu");
+  var menuBtn = document.getElementById("myNavMenu");
 
-    if(menuBtn.className === "nav-menu"){
-      menuBtn.className += " responsive";
-    } else {
-      menuBtn.className = "nav-menu";
-    }
+  if(menuBtn.className === "nav-menu"){
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
   }
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-  window.onscroll = function() {headerShadow()};
+window.onscroll = function() {headerShadow()};
 
-  function headerShadow() {
-    const navHeader =document.getElementById("header");
+function headerShadow() {
+  const navHeader =document.getElementById("header");
 
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
 
-      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-      navHeader.style.height = "70px";
-      navHeader.style.lineHeight = "70px";
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
 
-    } else {
+  } else {
 
-      navHeader.style.boxShadow = "none";
-      navHeader.style.height = "90px";
-      navHeader.style.lineHeight = "90px";
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
 
-    }
   }
+}
 
 
 /* ----- TYPING EFFECT ----- */
- var typingEffect = new Typed(".typedText",{
-    strings : ["Frontend Developer"],
-    loop : true,
-    typeSpeed : 100, 
-    backSpeed : 80,
-    backDelay : 2000
- })
+var typingEffect = new Typed(".typedText",{
+  strings : ["Programmer","Frontend Dev."],
+  loop : true,
+  typeSpeed : 100, 
+  backSpeed : 80,
+  backDelay : 2000
+})
 
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
- const sr = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 2000,
-        reset: true     
- })
+const sr = ScrollReveal({
+      origin: 'top',
+      distance: '80px',
+      duration: 2000,
+      reset: true     
+})
 
 /* -- HOME -- */
 sr.reveal('.featured-text-card',{})
@@ -68,27 +68,26 @@ sr.reveal('.top-header',{})
 
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
-  origin: 'left',
-  distance: '80px',
-  duration: 2000,
-  reset: true
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
 })
 
 srLeft.reveal('.about-info',{delay: 100})
 srLeft.reveal('.contact-info',{delay: 100})
-srLeft.reveal('#skill-left',{delay: 100})
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
-  origin: 'right',
-  distance: '80px',
-  duration: 2000,
-  reset: true
+origin: 'right',
+distance: '80px',
+duration: 2000,
+reset: true
 })
 
-srRight.reveal('#skill-right',{delay: 100})
+srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
-srRight.reveal('.info-box',{delay: 100})
+
 
 
 /* ----- CHANGE ACTIVE LINK ----- */
@@ -96,23 +95,23 @@ srRight.reveal('.info-box',{delay: 100})
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-  const scrollY = window.scrollY;
+const scrollY = window.scrollY;
 
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
+sections.forEach(current =>{
+  const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute('id')
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
 
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
 
-    }  else {
+  }  else {
 
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
 
-    }
-  })
+  }
+})
 }
 
 window.addEventListener('scroll', scrollActive)
